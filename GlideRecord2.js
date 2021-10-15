ebce3b57201380fbcf1e8d5e7278205f015a4d0d
DirkRedeker <42168076+DirkRedeker@users.noreply.github.com>
2021-10-15 13:13:40 +0200

    Commit 3 in "Branch-B-2"

--- GlideRecord2.js
+++ GlideRecord2.js
@@ -1,7 +1,5 @@
-// Changes in "Branch-B-2"
-// Und noch eine Änderung im "Branch-B-2"
 var gr = new GlideRecord('incident');
-
+//Hier
 gr.addQuery('number', 'INC0010001');
 gr.query();
 if (gr.next()) {
